b6f6f1ba56d639279658be26f3269899e719f712
Marcus Nielsen <me@marcusnielsen.org>
2019-07-15 17:29:18 +0200

    add use-store

---
+++ src/use-store.ts
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react";
+import { Observable } from "rxjs";
+
+export function useStore<Store>(
+  initialStore: Store,
+  storeStream: Observable<Store>
+) {
+  const [store, setStore] = useState(initialStore);
+
+  useEffect(() => {
+    const subscription = storeStream.subscribe(store => {
+      setStore(store);
+    });
+
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, []);
+
+  return store;
+}
